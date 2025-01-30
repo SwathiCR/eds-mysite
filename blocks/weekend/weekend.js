88e1f4dfba1d777c489995f782cac2723d2f467f
schagalli <schagalli@adobe.com>
2025-01-30 10:34:31 +0530

    Added the code for adding classname

--- blocks/weekend/weekend.js
+++ blocks/weekend/weekend.js
@@ -1,6 +1,9 @@
 import { fetchPlaceholders } from '../../scripts/aem.js';
 
-export default async function decorate() {
+export default async function decorate(block) {
+  [...block.children].forEach((row) => {
+    row.classList.add('places');
+  });
   const placeholders = await fetchPlaceholders();
   const clickMe = placeholders.ClickHere;
   console.log(clickMe);
